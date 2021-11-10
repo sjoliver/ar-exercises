5928c17af081bb6793d99a7b03726c6c8a1f0543
Sophie <sophiejoliver19+1@gmail.com>
2021-11-10 03:54:13 +0000

    exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,20 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+Store.has_many :employees
+Employee.belongs_to :store
+
+@store1.employees.create(first_name: "Sophie", last_name: "Oliver", hourly_rate: 420)
+@store1.employees.create(first_name: "Russell", last_name: "Engebretson", hourly_rate: 420)
+@store1.employees.create(first_name: "Katie", last_name: "Herda", hourly_rate: 420)
+@store1.employees.create(first_name: "Frankie", last_name: "Oliver", hourly_rate: 420)
+
+@store2.employees.create(first_name: "Katie", last_name: "Oliver", hourly_rate: 420)
+@store2.employees.create(first_name: "Sophie", last_name: "Engebretson", hourly_rate: 420)
+@store2.employees.create(first_name: "Frankie", last_name: "Herda", hourly_rate: 420)
+@store2.employees.create(first_name: "Russell", last_name: "Oliver", hourly_rate: 420)
+
+@store3.employees.create(first_name: "Russell", last_name: "Oliver", hourly_rate: 420)
+@store3.employees.create(first_name: "Frankie", last_name: "Engebretson", hourly_rate: 420)
+@store3.employees.create(first_name: "Sophie", last_name: "Herda", hourly_rate: 420)
+@store3.employees.create(first_name: "Katie", last_name: "Oliver", hourly_rate: 420)
